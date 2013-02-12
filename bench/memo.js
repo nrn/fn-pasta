28c4af572cebb3313031617f2dcb2b9ff50079e5
Nick Niemeir <nick.niemeir@gmail.com>
2013-02-11 22:09:59 -0700

    Make the same as case bench

--- bench/memo.js
+++ bench/memo.js
@@ -8,38 +8,36 @@ function fib (x) {
   else return fib(x-1) + fib(x-2)
 }
 
-bench.add('Fast Memo', function () {
-  /*
-   * * memoize.js
-   * * by @philogb and @addyosmani
-   * * with further optimizations by @mathias
-   * * and @DmitryBaranovsk
-   * * perf tests: http://bit.ly/q3zpG3
-   * * Released under an MIT license.
-   * */
-  function memoize( fn ) {
-    return function () {
-      var args = Array.prototype.slice.call(arguments),
-        hash = "",
-        i = args.length;
-      currentArg = null;
-      while (i--) {
-        currentArg = args[i];
-        hash += (currentArg === Object(currentArg)) ?
-        JSON.stringify(currentArg) : currentArg;
-        fn.memoize || (fn.memoize = {});
-      }
-      return (hash in fn.memoize) ? fn.memoize[hash] :
-        fn.memoize[hash] = fn.apply(this, args);
-    };
-  }
+/* * * memoize.js
+ * * by @philogb and @addyosmani
+ * * with further optimizations by @mathias
+ * * and @DmitryBaranovsk
+ * * perf tests: http://bit.ly/q3zpG3
+ * * Released under an MIT license.
+ * */
+function memoize( fn ) {
+  return function () {
+    var args = Array.prototype.slice.call(arguments),
+      hash = "",
+      i = args.length;
+    currentArg = null;
+    while (i--) {
+      currentArg = args[i];
+      hash += (currentArg === Object(currentArg)) ?
+      JSON.stringify(currentArg) : currentArg;
+      fn.memoize || (fn.memoize = {});
+    }
+    return (hash in fn.memoize) ? fn.memoize[hash] :
+      fn.memoize[hash] = fn.apply(this, args);
+  };
+}
 
-  memoize(fib)(20)
-
-}).add('My Memo', function () {
+bench.add('My Memo', function () {
   p.memo(fib)(20)
+}).add('Fast Memo', function () {
+  memoize(fib)(20)
 }).on('cycle', function (e) {
-  console.log(JSON.stringify(e.target.count, null, 2))
+  console.log(e.target.count + ' ' + e.target.name)
 }).on('complete', function () {
   console.log('Fastest is ' + this.filter('fastest').pluck('name'))
 }).run({ async: true })
